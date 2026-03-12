af2d8172e8c5f8bb5bde67a9597a5a66eb7d8a56
priyan21-web <priyadarshan2107@gmail.com>
2026-03-12 18:11:40 +0530

    UC3 display hello with name

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,12 +2,15 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
+        String name;
+
+        // Check if argument is provided
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
+            name = args[0];
         } else {
-            System.out.println("Hello User");
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
